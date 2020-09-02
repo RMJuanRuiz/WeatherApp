e30ca91cdce41a3b34aa31faf509736ee6d7b1ab
Juan Ruiz <rmjuanruiz@gmail.com>
2020-09-02 11:47:50 -0500

    Working on forecastComponent

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -7,16 +7,46 @@ import { WeatherService } from 'src/app/services/weather.service';
   styleUrls: ['./forecast.component.scss'],
 })
 export class ForecastComponent implements OnInit {
-  public forecastWeather: any;
+  public daysForecastArray: [];
 
-  constructor(public _weatherService: WeatherService) {}
+  constructor(public _weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    const days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
     this._weatherService
       .getCityWeatherForecast(4.60971, -74.08175)
       .subscribe((resp) => {
-        this.forecastWeather = resp;
-        console.log(resp);
+        const next3Days = resp['daily'].slice(1, 4);
+
+        this.daysForecastArray = next3Days.map((day: any, index: number) => {
+          let dayForecast: {
+            day: string;
+            maxTemp: number;
+            minTemp: number;
+            weather: string;
+            weatherIcon: string;
+          };
+
+          dayForecast = {
+            day: days[new Date().getDay() + (index + 1)],
+            maxTemp: Math.round(day.temp.max),
+            minTemp: Math.round(day.temp.min),
+            weather: day.weather[0].main,
+            weatherIcon: `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`,
+          };
+
+          return dayForecast;
+        });
+
       });
   }
 }
--- src/app/components/main-city/main-city.component.ts
+++ src/app/components/main-city/main-city.component.ts
@@ -23,36 +23,7 @@ export class MainCityComponent implements OnInit {
       .subscribe((resp) => {
         this.cityTemp = resp['main'].temp;
         this.weather = resp['weather'][0].main;
-
-        switch (this.weather) {
-          case 'Thunderstorm':
-            this.weatherURL = this.getIconURL('11d');
-            break;
-
-          case 'Drizzle':
-            this.weatherURL = this.getIconURL('09d');
-            break;
-
-          case 'Rain':
-            this.weatherURL = this.getIconURL('10d');
-            break;
-
-          case 'Snow':
-            this.weatherURL = this.getIconURL('13d');
-            break;
-
-          case 'Clear':
-            this.weatherURL = this.getIconURL('01d');
-            break;
-
-          case 'Clouds':
-            this.weatherURL = this.getIconURL('02d');
-            break;
-
-          default:
-            this.weatherURL = this.getIconURL('50d');
-            break;
-        }
+        this.weatherURL = this.getIconURL(resp['weather'][0].icon);
       });
   }
 }
